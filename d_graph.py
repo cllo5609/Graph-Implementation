5a696620f7590f4e67de7d11fb844dbe91bb0b18
Clint Lohr <lohrcl@oregonstate.edu>
2023-03-13 18:04:55 -0600

    Update d_graph.py

--- d_graph.py
+++ d_graph.py
@@ -1,10 +1,5 @@
 # Author: Clint Lohr
-# Assignment 6: Directed Graph
-# Description: A class to implement a directed graph. Vertices and their connected vertices are stored using an
-# adjacency matrix in the form of a list. The class contains methods for adding and removing edges and vertexes,
-# checking the graph for valid paths, cycles, performs depth-first searches and breadth-first, and performs
-# Dijkstra's Algorithm to find the shortest cost/distance path in the graph.
-# searches.
+# Directed Graph
 
 
 import heapq
